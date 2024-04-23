5e45140b70c6e6cf82f9a72db1c14881e96eb1f7
kiu_shubitidze.sandro <shubitidze.sandro@kiu.edu.ge>
2024-04-24 00:49:49 +0400

    Adding Video Calling, Setup and Meeting Rooms

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,17 +1,22 @@
 import Navbar from '@/components/ui/Navbar'
 import SideBar from '@/components/ui/SideBar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
-
-
-
+export const metadata: Metadata = {
+    title: "YOOM",
+    description: "Video calling app",
+    icons: {
+        icon: '/icons/logo.svg'
+    }
+};
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
     return (
         <main className='relative'>
-            <Navbar/>
+            <Navbar />
             <div className='flex'>
-                <SideBar/>
+                <SideBar />
                 <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
                     <div className='w-full'>
                         {children}
